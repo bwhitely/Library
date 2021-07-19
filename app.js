519c93994a1fb15c3054f64e53eb0142e86089fc
BWhitely <49163684+bwhitely@users.noreply.github.com>
2021-07-19 15:57:52 +0930

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 let library = [];
 
 // Book object constructor
-function Book(title, author, pages, read){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor(title, author, pages, read){
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
 }
 
 // Document elements
